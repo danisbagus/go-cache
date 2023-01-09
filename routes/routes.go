fc9599f4023258cddb30b48d2f97f0408502866e
Danis Bagus Setiawan <danis.bagus@majoo.id>
2023-01-09 16:52:38 +0700

    implement cache

--- routes/routes.go
+++ routes/routes.go
@@ -4,13 +4,15 @@ import (
 	"github.com/danisbagus/go-cache/handler"
 	"github.com/danisbagus/go-cache/repository"
 	"github.com/danisbagus/go-cache/service"
+	"github.com/danisbagus/go-cache/utils/cache"
 	"github.com/labstack/echo/v4"
 )
 
 func ApiRoutes(route *echo.Echo) {
+	cache := cache.NewCache()
 	ratesRepository := repository.NewRatesRepository()
 
-	ratesService := service.NewRatesService(ratesRepository)
+	ratesService := service.NewRatesService(ratesRepository, cache)
 
 	rateseHandler := handler.NewRatesHandler(ratesService)
 
--- service/rates.go
+++ service/rates.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"time"
+
 	"github.com/danisbagus/go-cache/domain"
 	"github.com/danisbagus/go-cache/repository"
+
+	"github.com/danisbagus/go-cache/utils/cache"
 )
 
 type (
@@ -11,24 +15,33 @@ type (
 	}
 
 	RatesService struct {
+		cache     *cache.Cache
 		ratesRepo repository.IRatesRepository
 	}
 )
 
-func NewRatesService(ratesRepo repository.IRatesRepository) IRatesService {
+func NewRatesService(ratesRepo repository.IRatesRepository, cache *cache.Cache) IRatesService {
 	return &RatesService{
 		ratesRepo: ratesRepo,
+		cache:     cache,
 	}
 }
 
 func (s *RatesService) GetUSDToIDRRates() (domain.Rates, error) {
 	var rates domain.Rates
 
+	value, found := s.cache.Get("IDR_RATES")
+	if found {
+		rates.IDR = value.(float64)
+		return rates, nil
+	}
+
 	IDRRates, err := s.ratesRepo.GetUSDToIDRRates()
 	if err != nil {
 		return rates, err
 	}
 
+	s.cache.Set("IDR_RATES", IDRRates, 1*time.Minute)
 	rates.IDR = IDRRates
 	return rates, nil
 }
